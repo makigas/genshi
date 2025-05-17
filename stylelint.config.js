e837c389cc2604d2f38f276b54847a67177c8e61
Dani Rodríguez <dani@danirod.es>
2025-05-17 17:15:37 +0200

    Run prettier as a stylelint step

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,9 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
   extends: ["stylelint-config-standard-scss", "stylelint-config-property-sort-order-smacss"],
-  plugins: ["stylelint-order"],
+  plugins: ["stylelint-prettier", "stylelint-order"],
   rules: {
+    "prettier/prettier": true,
     "selector-class-pattern": [
       "^([a-z][a-z0-9]*)(-[a-z0-9]+)*|Genshi([A-Z])([A-Za-z0-9]*)$",
       {
